495857f733515582e675138811b40135319f95af
Iman <imanghafoori1@gmail.com>
2020-09-18 08:45:58 +0430

    some fixes from laravel-microscope

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -76,7 +76,6 @@ class Kernel extends HttpKernel
     protected $middlewarePriority = [
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \App\Http\Middleware\Authenticate::class,
         \Illuminate\Routing\Middleware\ThrottleRequests::class,
         \Illuminate\Session\Middleware\AuthenticateSession::class,
         \Illuminate\Routing\Middleware\SubstituteBindings::class,
--- tests/Unit/TagTest.php
+++ tests/Unit/TagTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Unit;
 
 use App\Package;
 use App\Tag;
