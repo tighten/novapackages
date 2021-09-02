4f7583117d69d4b9e2e537f5a3989d2a1a04796c
Matt Stauffer <matt@tighten.co>
2021-09-02 09:57:19 -0400

    Change default queue failed driver to database instead of database-uuid

--- config/queue.php
+++ config/queue.php
@@ -81,7 +81,7 @@ return [
     */
 
     'failed' => [
-        'driver' => env('QUEUE_FAILED_DRIVER', 'database-uuids'),
+        'driver' => env('QUEUE_FAILED_DRIVER', 'database'),
         'database' => env('DB_CONNECTION', 'mysql'),
         'table' => 'failed_jobs',
     ],
