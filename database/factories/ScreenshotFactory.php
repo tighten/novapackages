47f25d57dd1baf5ffcf9ec7facaf7bd564946583
Shift <shift@laravelshift.com>
2022-05-25 17:48:38 +0000

    Remove unnecessary `$model` property

--- database/factories/ScreenshotFactory.php
+++ database/factories/ScreenshotFactory.php
@@ -9,13 +9,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class ScreenshotFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Screenshot::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array
