e823a820ab2845d555f32cc0cf8ccede7a369569
Marcus Moore <contact@marcusmoore.io>
2022-07-20 16:25:30 -0700

    Stub GitHub API integration test

---
+++ tests/Integration/GitHubApiTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Integration;
+
+class GitHubApiTest
+{
+    /** @test */
+    function readme_response_in_expected_format()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /** @test */
+    function releases_response_in_expected_format()
+    {
+        $this->markTestIncomplete();
+    }
+}
