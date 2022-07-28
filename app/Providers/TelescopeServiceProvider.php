b5b150c9d228de76d1070e43cc5eb59ceb1ac7a5
Marcus Moore <contact@marcusmoore.io>
2022-07-28 14:04:28 -0700

    Allow admins to view telescope dashboard

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -63,9 +63,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewTelescope', function ($user) {
-            return in_array($user->email, [
-                'matt@tighten.co',
-            ]);
+            return $user->isAdmin();
         });
     }
 }
