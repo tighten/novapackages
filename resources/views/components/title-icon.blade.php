9146b9cd228494fac3f5e545eeb69c23e5618661
Marcus Moore <contact@marcusmoore.io>
2022-08-16 11:53:22 -0700

    Create basic title icon blade component

---
+++ resources/views/components/title-icon.blade.php
@@ -0,0 +1,25 @@
+@props([
+    'size',
+    'title',
+])
+
+<div
+    @class([
+        'h-8 w-8 mr-2 text-md' => $size === 'small',
+        'h-10 w-10 mr-4 text-md' => $size === 'medium',
+        'h-16 w-16 mr-4 text-lg' => $size === 'large',
+        'h-24 w-24 mr-6 text-xl' => $size === 'xlarge',
+    ])
+>
+    <div
+        @class([
+            'rounded-full inline-block bg-slate-500 text-white text-center leading-none flex flex-row items-center justify-center',
+            'h-8 w-8 mr-2 text-md' => $size === 'small',
+            'h-10 w-10 mr-4 text-md' => $size === 'medium',
+            'h-16 w-16 mr-4 text-lg' => $size === 'large',
+            'h-24 w-24 mr-6 text-xl' => $size === 'xlarge',
+        ])
+    >
+        {{ str($title)->substr(0, 1)->upper() }}
+    </div>
+</div>
