4c81ee0617de5a8576c2d0f0404b26bc193b2424
Marcus Moore <contact@marcusmoore.io>
2022-08-16 11:54:15 -0700

    WIP: Begin to extract title and install box from package show component

--- resources/views/packages/show.blade.php
+++ resources/views/packages/show.blade.php
@@ -17,6 +17,38 @@
 @section('content')
     <x-status class="container mx-auto mb-4"/>
 
+    <div
+        id="top"
+        class="container mx-auto flex flex-col items-center px-2"
+    >
+        <div class="w-full">
+            <button
+                type="button"
+                class="self-start text-indigo-600 font-bold text-sm uppercase no-underline inline-block text-center hover:text-indigo-900 cursor-pointer"
+                {{--@click="goBack"--}}
+            >
+                &#8592; Back
+            </button>
+        </div>
+
+        <div class="w-full flex flex-col md:flex-row items-center justify-between pt-2 md:pt-6 md:pb-8">
+            <div class="flex flex-row items-center mb-4 sm:mb-0">
+                <x-title-icon :title="$package['name']" size="large" />
+
+                <h1 class="inline text-gray-800 text-2xl md:text-4xl font-bold">
+                    {{ $package['name'] }}
+                    @if ($package['is_disabled'])
+                        <span class="text-xs uppercase text-red">
+                            Disabled
+                        </span>
+                    @endif
+                </h1>
+            </div>
+
+            <x-install-box />
+        </div>
+    </div>
+
     <package-detail-frame
         :auth="{{ auth()->check() ? 'true' : 'false' }}"
         :auth-id="{{ auth()->check() ? auth()->id() : 'null' }}"
