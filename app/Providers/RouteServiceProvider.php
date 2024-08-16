9c60e5c9f2cad81911ae0bf16c053bf1d27e8089
Omar Rida <omar.rida@tighten.co>
2024-08-16 11:26:22 +0300

    change home route to /

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, etc.
