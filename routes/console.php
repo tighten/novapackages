229bfd4b1cb9992ecf91f40d043fb7dd67748cee
Shift <shift@laravelshift.com>
2025-03-06 00:11:52 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::command('sync:packagist')->everyTwoHours();
+// Every two hours at minute 30.
+Schedule::command('sync:repo')->cron('30 */2 * * *');
+Schedule::command('purge:abandonedscreenshots')->dailyAt('1:00');
+Schedule::command('telescope:prune')->daily();
+
+// Schedule::command('novapackages:check-package-urls')->weeklyOn(7, '20:00');
+// Schedule::command('novapackages:send-unavailable-package-followup')->dailyAt('21:00');
+// Schedule::command('novapackages:disable-unavailable-packages')->dailyAt('21:30');
