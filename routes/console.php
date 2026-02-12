0531b506b2ee6182aa22bdd873f39f21d3b10169
Matt Stauffer <matt@tighten.co>
2026-02-11 21:51:05 -0500

    Merge resolution

--- routes/console.php
+++ routes/console.php
@@ -13,8 +13,6 @@ Schedule::command('sync:packagist')->everyTwoHours();
 // Every two hours at minute 30.
 Schedule::command('sync:repo')->cron('30 */2 * * *');
 Schedule::command('purge:abandonedscreenshots')->dailyAt('1:00');
-Schedule::command('telescope:prune')->daily();
-
 // Schedule::command('novapackages:check-package-urls')->weeklyOn(7, '20:00');
 // Schedule::command('novapackages:send-unavailable-package-followup')->dailyAt('21:00');
 // Schedule::command('novapackages:disable-unavailable-packages')->dailyAt('21:30');
