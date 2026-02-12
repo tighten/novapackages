1075c2586d8ac776be23c906e509dee294b46702
Matt Stauffer <matt@tighten.co>
2026-02-11 21:22:48 -0500

    Fix more tests

--- resources/views/livewire/package-list.blade.php
+++ resources/views/livewire/package-list.blade.php
@@ -1,68 +1,77 @@
-@extends('livewire.layouts.home-page')
+<div class="relative">
+    <div class="flex flex-col container mx-auto pb-8">
+        <p class="block text-center text-3xl sm:text-4xl mb-10 mx-4 text-gray-900">Discover new packages. <br class="md:hidden block" />Build amazing things.</p>
 
-@section('home-page-body')
-<div class="w-full">
-    @if ($tag === 'all')
-        <h2 class="ml-2 mb-2 text-2xl font-bold">All Packages (newest first)</h2>
-    @else
-        <h2 class="ml-2 mb-6 text-2xl font-bold">Tag: {{ $tag }}</h2>
-    @endif
-    @if ($search)
-        <div class="ml-2 mb-2">Filtered by search query "{{ $search }}"</div>
-    @endif
+        @include('livewire.partials.search')
 
-    @if (! $packages->isEmpty())
-        <div wire:key="package-bounding-box" class="flex flex-wrap justify-center sm:justify-start mb-6">
-            @each('livewire.partials.package-card', $packages, 'package')
-        </div>
-    @else
-        <div class="block w-full font-bold text-xl text-gray-800 self-start ml-2 mt-8">
-            Sorry, but no packages currently in our database match this filter.
-        </div>
-    @endif
+        <!-- Flexes the left/right column of the whole page layout -->
+        <div class="flex flex-col sm:flex-row justify-around sm:mt-8">
+            @include('livewire.partials.package-list-tags')
 
-    @if ($packages->hasPages())
-    <div class="md:flex mb-12">
-        <div class="flex-initial mr-8">
-            {!! $packages->links() !!}
-        </div>
-        <div x-data="{ 'pageSize': {{ $pageSize }}, 'open': false }" class="inline-block md:flex-initial ml-4 md:ml-0 mt-4 md:mt-0 relative text-gray-700">
-            <span class="inline-block mr-2">Rows per page:</span>
-            <div class="hover:bg-white inline-block border border-gray-400 cursor-pointer p-2 rounded-sm" x-on:click="open = true" :class="{ 'bg-white': open}">
-                <span class="inline-block" x-text="pageSize"></span>
-                <svg viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="inline-block fill-current text-gray-700 h-4 w-4">
-                    <g stroke="none" stroke-width="1" fill-rule="evenodd">
-                        <g>
-                            <polygon points="9.29289322 12.9497475 10 13.6568542 15.6568542 8 14.2426407 6.58578644 10 10.8284271 5.75735931 6.58578644 4.34314575 8"></polygon>
-                        </g>
-                    </g>
-                </svg>
-            </div>
+            <div class="w-full">
+                @if ($tag === 'all')
+                    <h2 class="ml-2 mb-2 text-2xl font-bold">All Packages (newest first)</h2>
+                @else
+                    <h2 class="ml-2 mb-6 text-2xl font-bold">Tag: {{ $tag }}</h2>
+                @endif
+                @if ($search)
+                    <div class="ml-2 mb-2">Filtered by search query "{{ $search }}"</div>
+                @endif
 
-            <div x-show="open" x-on:click.away="open = false" class="absolute bg-white border border-gray-400 mt-9 top-0 w-full" x-cloak>
-                @foreach ([6, 12, 18, 24] as $thisPageSize)
-                <a href="#"
-                    class="block {{ $pageSize == $thisPageSize ? 'font-bold' : '' }} hover:bg-gray-100 hover:text-gray-900 p-2 text-center"
-                    wire:click.prevent="changePageSize({{ $thisPageSize }})">{{ $thisPageSize }}</a>
-                @endforeach
+                @if (! $packages->isEmpty())
+                    <div wire:key="package-bounding-box" class="flex flex-wrap justify-center sm:justify-start mb-6">
+                        @each('livewire.partials.package-card', $packages, 'package')
+                    </div>
+                @else
+                    <div class="block w-full font-bold text-xl text-gray-800 self-start ml-2 mt-8">
+                        Sorry, but no packages currently in our database match this filter.
+                    </div>
+                @endif
+
+                @if ($packages->hasPages())
+                <div class="md:flex mb-12">
+                    <div class="flex-initial mr-8">
+                        {!! $packages->links() !!}
+                    </div>
+                    <div x-data="{ 'pageSize': {{ $pageSize }}, 'open': false }" class="inline-block md:flex-initial ml-4 md:ml-0 mt-4 md:mt-0 relative text-gray-700">
+                        <span class="inline-block mr-2">Rows per page:</span>
+                        <div class="hover:bg-white inline-block border border-gray-400 cursor-pointer p-2 rounded-sm" x-on:click="open = true" :class="{ 'bg-white': open}">
+                            <span class="inline-block" x-text="pageSize"></span>
+                            <svg viewBox="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="inline-block fill-current text-gray-700 h-4 w-4">
+                                <g stroke="none" stroke-width="1" fill-rule="evenodd">
+                                    <g>
+                                        <polygon points="9.29289322 12.9497475 10 13.6568542 15.6568542 8 14.2426407 6.58578644 10 10.8284271 5.75735931 6.58578644 4.34314575 8"></polygon>
+                                    </g>
+                                </g>
+                            </svg>
+                        </div>
+
+                        <div x-show="open" x-on:click.away="open = false" class="absolute bg-white border border-gray-400 mt-9 top-0 w-full" x-cloak>
+                            @foreach ([6, 12, 18, 24] as $thisPageSize)
+                            <a href="#"
+                                class="block {{ $pageSize == $thisPageSize ? 'font-bold' : '' }} hover:bg-gray-100 hover:text-gray-900 p-2 text-center"
+                                wire:click.prevent="changePageSize({{ $thisPageSize }})">{{ $thisPageSize }}</a>
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+                @endif
             </div>
         </div>
     </div>
-    @endif
-</div>
-@endsection
+    <div wire:loading style="top: 7em; width: 10em; left: 50%; margin-left: -5em" class="absolute bg-indigo-900 rounded-lg text-4xl text-center text-white tracking-wide py-6 w-full uppercase w-4">Loading...</div>
 
-@push('scripts')
+    @script
     <script>
         document.onkeydown = function(e) {
             e = e || window.event;
 
             switch(e.which || e.keyCode) {
                 case 37: // left
-                    @this.call('previousPage');
+                    $wire.call('previousPage');
                 break;
                 case 39: // right
-                    @this.call('nextPage');
+                    $wire.call('nextPage');
                 break;
 
                 default: return;
@@ -71,4 +80,5 @@
             e.preventDefault();
         }
     </script>
-@endpush
+    @endscript
+</div>
--- resources/views/livewire/popular-and-recent-packages.blade.php
+++ resources/views/livewire/popular-and-recent-packages.blade.php
@@ -1,22 +1,31 @@
-@extends('livewire.layouts.home-page')
+<div class="relative">
+    <div class="flex flex-col container mx-auto pb-8">
+        <p class="block text-center text-3xl sm:text-4xl mb-10 mx-4 text-gray-900">Discover new packages. <br class="md:hidden block" />Build amazing things.</p>
 
-@section('home-page-body')
-<div class="w-full">
-    <h2 class="ml-2 mb-2 text-2xl font-bold">Recent</h2>
-    <div class="flex flex-wrap justify-center sm:justify-start">
-        @foreach ($recentPackages as $package)
-            @include('livewire.partials.package-card', ['context' => 'recent'])
-        @endforeach
+        @include('livewire.partials.search')
+
+        <!-- Flexes the left/right column of the whole page layout -->
+        <div class="flex flex-col sm:flex-row justify-around sm:mt-8">
+            @include('livewire.partials.package-list-tags')
+
+            <div class="w-full">
+                <h2 class="ml-2 mb-2 text-2xl font-bold">Recent</h2>
+                <div class="flex flex-wrap justify-center sm:justify-start">
+                    @foreach ($recentPackages as $package)
+                        @include('livewire.partials.package-card', ['context' => 'recent'])
+                    @endforeach
+                </div>
+                <a href="#" wire:click.prevent="filterTag('all')" class="text-indigo-600 underline font-bold ml-2 mb-6">See More...</a>
+
+                <h2 class="ml-2 mb-2 mt-8 text-2xl font-bold">Popular</h2>
+                <div class="flex flex-wrap justify-center sm:justify-start">
+                    @foreach ($popularPackages as $package)
+                        @include('livewire.partials.package-card', ['context' => 'popular'])
+                    @endforeach
+                </div>
+                <a href="#" wire:click.prevent="filterTag('popular')" class="text-indigo-600 underline font-bold ml-2 mb-6">See More...</a>
+            </div>
+        </div>
     </div>
-    <a href="#" wire:click.prevent="filterTag('all')" class="text-indigo-600 underline font-bold ml-2 mb-6">See More...</a>
-
-    <h2 class="ml-2 mb-2 mt-8 text-2xl font-bold">Popular</h2>
-    <div class="flex flex-wrap justify-center sm:justify-start">
-        @foreach ($popularPackages as $package)
-            @include('livewire.partials.package-card', ['context' => 'popular'])
-        @endforeach
-
-    </div>
-    <a href="#" wire:click.prevent="filterTag('popular')" class="text-indigo-600 underline font-bold ml-2 mb-6">See More...</a>
+    <div wire:loading style="top: 7em; width: 10em; left: 50%; margin-left: -5em" class="absolute bg-indigo-900 rounded-lg text-4xl text-center text-white tracking-wide py-6 w-full uppercase w-4">Loading...</div>
 </div>
-@endsection
