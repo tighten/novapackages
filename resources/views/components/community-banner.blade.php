8e463969b218caab82253047bb230cf04fc1b7f3
Matt Stauffer <matt@tighten.co>
2026-02-11 21:39:28 -0500

    Add community banner

---
+++ resources/views/components/community-banner.blade.php
@@ -0,0 +1,11 @@
+<div class="w-full bg-black text-white text-sm py-2">
+    <div class="mx-auto max-w-[1440px] px-4 py-2 flex items-center justify-between">
+        <span>Laravel Community Tools by <a href="https://tighten.com" class="hover:underline">Tighten</a></span>
+
+        <nav class="flex items-center gap-4">
+            <a href="https://novapackages.com" class="text-indigo-500 hover:underline">Nova Packages</a>
+            <a href="https://laravel-tricks.com" class="hover:underline">Laravel Tricks</a>
+            <a href="https://forgerecipes.com" class="hover:underline">Forge Recipes</a>
+        </nav>
+    </div>
+</div>
